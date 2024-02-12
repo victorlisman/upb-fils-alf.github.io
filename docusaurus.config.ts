f460936bc2333877d1193ed90261dea906030c22
Alexandru RADOVICI <alexandru.radovici@wyliodrin.com>
2024-02-12 17:58:50 +0200

    update license

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -122,7 +122,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} UNST Politehnica Bucharest.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Politehnica Bucharest.`,
     },
     prism: {
       theme: prismThemes.github,

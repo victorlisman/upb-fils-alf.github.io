63d8dc689a1614d857631aeba311c53666b2a5c1
Mihai Costin <101063308+mihaicostin34@users.noreply.github.com>
2024-02-20 16:55:38 +0200

    TP 02 first iteration (#13)
    * TP 02 first iteration
    
    * Added explanations and examples
    
    * Modified code samples

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -127,6 +127,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ['nasm'],
     },
   } satisfies Preset.ThemeConfig,
 };

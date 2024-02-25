63116d74054f81f78a29ae3ed5ef85d012a1d413
Alexandru RADOVICI <alexandru.radovici@wyliodrin.com>
2024-02-25 23:27:47 +0200

    social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -70,7 +70,7 @@ const config: Config = {
   ],
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/alf-social-card.png',
     navbar: {
       title: 'Automates, Langages Formels et Compilateurs',
       logo: {
